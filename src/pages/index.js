861556cdcc84b79e547fbcd3835c13eebf370f50
hunglq <hunglq@vietcas.vn>
2019-09-17 16:25:50 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React, { PureComponent } from 'react'
 import Redirect from 'umi/redirect'
 import { withI18n } from '@lingui/react'
-import Home from './home/Components'
 
 @withI18n()
 class Index extends PureComponent {
   render() {
     const { i18n } = this.props
-    // return <Redirect to={i18n.t`/dashboard`} />
-    return <Home />
+    return <Redirect to={i18n.t`/dashboard`} />
   }
 }
 
