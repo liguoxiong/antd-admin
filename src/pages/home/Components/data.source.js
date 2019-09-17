fbce2817277b8c5b169d78be3ac03771da4ecacc
hunglq <hunglq@vietcas.vn>
2019-09-17 11:32:31 +0700

    update homepage  Changes to be committed: 	modified:   package.json 	new file:   src/pages/home/Components/Banner0.jsx 	new file:   src/pages/home/Components/Banner1.jsx 	new file:   src/pages/home/Components/Content0.jsx 	new file:   src/pages/home/Components/Content3.jsx 	new file:   src/pages/home/Components/Content5.jsx 	new file:   src/pages/home/Components/FeedbackForm.js 	new file:   src/pages/home/Components/Footer1.jsx 	new file:   src/pages/home/Components/Nav0.jsx 	new file:   src/pages/home/Components/data.source.js 	new file:   src/pages/home/Components/documentation.md 	new file:   src/pages/home/Components/index.jsx 	new file:   src/pages/home/Components/less/antMotionStyle.less 	new file:   src/pages/home/Components/less/banner0.less 	new file:   src/pages/home/Components/less/banner1.less 	new file:   src/pages/home/Components/less/common.less 	new file:   src/pages/home/Components/less/content.less 	new file:   src/pages/home/Components/less/content0.less 	new file:   src/pages/home/Components/less/content3.less 	new file:   src/pages/home/Components/less/content5.less 	new file:   src/pages/home/Components/less/custom.less 	new file:   src/pages/home/Components/less/feedbackForm.less 	new file:   src/pages/home/Components/less/footer1.less 	new file:   src/pages/home/Components/less/nav0.less 	new file:   src/pages/home/Components/utils.js 	new file:   src/pages/home/themes/default.less 	new file:   src/pages/home/themes/index.less 	new file:   src/pages/home/themes/mixin.less 	new file:   src/pages/home/themes/vars.less 	modified:   src/pages/index.js

---
+++ src/pages/home/Components/data.source.js
@@ -0,0 +1,619 @@
+import React from 'react'
+export const Nav00DataSource = {
+  wrapper: { className: 'header0 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header0-logo',
+    children: 'https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg',
+  },
+  Menu: {
+    className: 'header0-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: 'Sản phẩm', name: 'text' }],
+        },
+        subItem: [
+          {
+            name: 'sub0',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Băng tải',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: 'Băng tải công nghiệp',
+                },
+              ],
+            },
+          },
+          {
+            name: 'sub1',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Máy tự động',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: 'Máy tự động',
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: 'item1',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: 'Dịch vụ', name: 'text' }],
+        },
+      },
+      {
+        name: 'item2',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: 'Giới thiệu', name: 'text' }],
+        },
+      },
+      {
+        name: 'item3',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: 'Liên hệ', name: 'text' }],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header0-mobile-menu' },
+}
+export const Banner01DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title',
+    children: 'http://bangtai.vn/upload/product/wp20140911001-5156.jpg',
+  },
+  content: {
+    className: 'banner0-content',
+    children: 'Băng tải tự động',
+  },
+  button: { className: 'banner0-button', children: 'Đặt mua' },
+}
+export const Banner10DataSource = {
+  wrapper: { className: 'banner1' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg0' },
+        title: {
+          className: 'banner1-title',
+          children: 'Máy may',
+        },
+        content: {
+          className: 'banner1-content',
+          children: 'Máy may giày da',
+        },
+        button: { className: 'banner1-button', children: 'Đặt hàng ngay' },
+      },
+      {
+        name: 'elem1',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg1' },
+        title: {
+          className: 'banner1-title',
+          children: 'Băng tải',
+        },
+        content: {
+          className: 'banner1-content',
+          children: 'Băng tải xích',
+        },
+        button: { className: 'banner1-button', children: 'Đặt hàng ngay' },
+      },
+      {
+        name: 'elem2',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg1' },
+        title: {
+          className: 'banner1-title',
+          children: '',
+        },
+        content: {
+          className: 'banner1-content',
+          children: 'Băng tải tự động',
+        },
+        button: { className: 'banner1-button', children: 'Đặt mua' },
+      },
+    ],
+  },
+}
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [{ name: 'title', children: 'DỊCH VỤ CỦA CHÚNG TÔI' }],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: 'Cung cấp băng tải',
+            },
+            {
+              name: 'content',
+              children: 'Chúng tôi cung cấp, lắp đặt các loại băng tải',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: 'Cung cấp máy may giày da',
+            },
+            {
+              name: 'content',
+              children:
+                'Chúng tôi cung cấp các loại máy may giày da phục vụ cho ngành giày da',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: 'Thi công lắp đặt',
+            },
+            {
+              name: 'content',
+              children: 'Thi công lắp đặt băng tải, hệ thống sản xuất',
+            },
+          ],
+        },
+      },
+    ],
+  },
+}
+export const Content50DataSource = {
+  wrapper: { className: 'home-page-wrapper content5-wrapper' },
+  page: { className: 'home-page content5' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      { name: 'title', children: 'SẢN PHẨM', className: 'title-h1' },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: 'Chúng tôi cung cấp các sản phẩm',
+      },
+    ],
+  },
+  block: {
+    className: 'content5-img-wrapper',
+    gutter: 16,
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://sc01.alicdn.com/kf/HTB1IleZRXXXXXa_apXXq6xXFXXXa/Sports-shoes-leather-sewing-machine.jpg_350x350.jpg',
+          },
+          content: { children: 'Máy may giày da' },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://sc01.alicdn.com/kf/HTB1IleZRXXXXXa_apXXq6xXFXXXa/Sports-shoes-leather-sewing-machine.jpg_350x350.jpg',
+          },
+          content: { children: 'Băng tải' },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://sc02.alicdn.com/kf/HTB1xro7ryOYBuNjSsD4q6zSkFXaD/JK810-Industrial-Sewing-Machine-For-Shoes-Used.jpg_350x350.jpg',
+          },
+          content: { children: 'Băng tải' },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children: 'http://maymayhiepthanh.com/upload/product/cs-335bh.jpg',
+          },
+          content: { children: 'Máy may' },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://vinatechnic.vn/uploads/shops/2014_10/bang-tai-chuyen-thung-carton.jpg',
+          },
+          content: { children: 'máy may' },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://www.bangtaihang.com/library/module_new/he-thong-bang-tai-trong-cong-nghiep_s1530.jpg',
+          },
+          content: { children: 'Băng tải' },
+        },
+      },
+      {
+        name: 'block6',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://bangtaicongnghiep.vn/uploads/Images/bang-tai-xich-1.jpg',
+          },
+          content: { children: 'Băng tải' },
+        },
+      },
+      {
+        name: 'block7',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://bangtaicongnghiep.vn/uploads/Images/bang-tai-xich-1.jpg',
+          },
+          content: { children: 'Máy may' },
+        },
+      },
+    ],
+  },
+}
+export const Content30DataSource = {
+  wrapper: { className: 'home-page-wrapper content3-wrapper' },
+  page: { className: 'home-page content3' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: 'TIN TỨC',
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: 'Tin tức liên quan đến ngành',
+      },
+    ],
+  },
+  block: {
+    className: 'content3-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: 'Lorem Ipsum' },
+          content: {
+            className: 'content3-content',
+            children:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu',
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: 'Lorem Ipsum' },
+          content: {
+            className: 'content3-content',
+            children:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu',
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: 'Lorem Ipsum' },
+          content: {
+            className: 'content3-content',
+            children:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu',
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: 'Lorem Ipsum' },
+          content: {
+            className: 'content3-content',
+            children:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu',
+          },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/UsUmoBRyLvkIQeO.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: 'Lorem Ipsum' },
+          content: {
+            className: 'content3-content',
+            children:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu',
+          },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: 'Lorem Ipsum' },
+          content: {
+            className: 'content3-content',
+            children:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu',
+          },
+        },
+      },
+    ],
+  },
+}
+export const Footer10DataSource = {
+  wrapper: { className: 'home-page-wrapper footer1-wrapper' },
+  OverPack: { className: 'footer1', playScale: 0.2 },
+  block: {
+    className: 'home-page',
+    gutter: 0,
+    children: [
+      {
+        name: 'block0',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          className: 'logo',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/qqaimmXZVSwAhpL.svg',
+        },
+        childWrapper: {
+          className: 'slogan',
+          children: [
+            {
+              name: 'content0',
+              children: 'Dịch vụ hàng đầu tại Việt Nam',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: 'Sản phẩm' },
+        childWrapper: {
+          children: [
+            { name: 'link0', href: '#', children: 'Băng tải tự động' },
+            { name: 'link1', href: '#', children: 'Máy may giày da' },
+            { name: 'link2', href: '#', children: 'Thiết bị phụ trợ' },
+            { name: 'link3', href: '#', children: 'Thiết bị thay thế' },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: 'Tin tức' },
+        childWrapper: {
+          children: [
+            { href: '#', name: 'link0', children: 'FAQ' },
+            { href: '#', name: 'link1', children: 'RSS' },
+          ],
+        },
+      },
+      {
+        name: 'block3',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: 'Liên kết' },
+        childWrapper: {
+          children: [
+            { href: '#', name: 'link0', children: 'Fanpage' },
+            { href: '#', name: 'link1', children: 'Đối tác' },
+          ],
+        },
+      },
+    ],
+  },
+  copyrightWrapper: { className: 'copyright-wrapper' },
+  copyrightPage: { className: 'home-page' },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <>
+        <span>
+          ©2019 by <a href="https://motion.ant.design">LVProduct</a> All Rights
+          Reserved
+        </span>
+      </>
+    ),
+  },
+}
